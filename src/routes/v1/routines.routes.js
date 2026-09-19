541b8790cf7f14d1a04fb8616450593e4ea79b95
YoiVA-1 <yoinervelez8@gmail.com>
2026-09-19 16:29:24 -0500

    feat(routines): actualización con PUT

--- src/routes/v1/routines.routes.js
+++ src/routes/v1/routines.routes.js
@@ -57,7 +57,22 @@ router.post('/', (req, res) => {
     res.status(201).json(newRoutine);
 });
 
-router.put('/:id', (req, res) => { });
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const index = routines.findIndex(r => r.id === Number(id));
+
+    if (index === -1) {
+        return res.status(404).json({ error: 'Rutina no encontrada' });
+    }
+
+    routines[index] = {
+        ...routines[index],
+        ...req.body
+    };
+
+    res.status(200).json(routines[index]);
+});
+
 router.delete('/:id', (req, res) => { });
 
 module.exports = router;
