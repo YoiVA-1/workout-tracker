47c527f1e493bcdbbbb5492ef9708e884511409a
YoiVA-1 <yoinervelez8@gmail.com>
2026-09-19 15:44:33 -0500

    feat(exercises): implementación de rutas GET (lista e individual)

--- src/routes/v1/exercises.routes.js
+++ src/routes/v1/exercises.routes.js
@@ -4,22 +4,36 @@ const router = Router();
 // Estado en memoria (simulación)
 let exercises = [
     {
-    id: 101,
-    name: "Press de Banca Plano",
-    description: "Ejercicio compuesto enfocado en el desarrollo del pectoral mayor y tríceps.",
-    category: {
-    id: 1,
-    name: "Pecho"
-    },
-    equipment_needed: "Barra y discos"
+        id: 101,
+        name: "Press de Banca Plano",
+        description: "Ejercicio compuesto enfocado en el desarrollo del pectoral mayor y tríceps.",
+        category: {
+            id: 1,
+            name: "Pecho"
+        },
+        equipment_needed: "Barra y discos"
     }
 ];
 
-// Rutas básicas (Stubs)
-router.get('/', (req, res) => {});
-router.get('/:id', (req, res) => {});
-router.post('/', (req, res) => {});
-router.put('/:id', (req, res) => {});
-router.delete('/:id', (req, res) => {});
+// GET /v1/exercises (Listar todo el catálogo)
+router.get('/', (req, res) => {
+    res.status(200).json(exercises);
+});
+
+// GET /v1/exercises/:id (Detalle de un ejercicio específico)
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const exercise = exercises.find(e => e.id === Number(id));
+
+    if (!exercise) {
+        return res.status(404).json({ error: 'Ejercicio no encontrado' });
+    }
+
+    res.status(200).json(exercise);
+});
+
+router.post('/', (req, res) => { });
+router.put('/:id', (req, res) => { });
+router.delete('/:id', (req, res) => { });
 
 module.exports = router;
