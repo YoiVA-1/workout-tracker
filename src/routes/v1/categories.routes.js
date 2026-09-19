417640ca134da607c47948e94ea7b368f92d4cf8
YoiVA-1 <yoinervelez8@gmail.com>
2026-09-19 18:32:49 -0500

    feat(categories): actualización con PUT

--- src/routes/v1/categories.routes.js
+++ src/routes/v1/categories.routes.js
@@ -55,7 +55,22 @@ router.post('/', (req, res) => {
     res.status(201).json(newCategory);
 });
 
-router.put('/:id', (req, res) => { });
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const index = categories.findIndex(c => c.id === Number(id));
+
+    if (index === -1) {
+        return res.status(404).json({ error: 'Categoría no encontrada' });
+    }
+
+    categories[index] = {
+        ...categories[index],
+        ...req.body
+    };
+
+    res.status(200).json(categories[index]);
+});
+
 router.delete('/:id', (req, res) => { });
 
 module.exports = router;
