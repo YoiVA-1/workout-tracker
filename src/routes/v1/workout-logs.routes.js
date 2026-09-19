9a7b0aafafab8736aa2543bc4a64dce0d6d0b91f
YoiVA-1 <yoinervelez8@gmail.com>
2026-09-19 18:25:41 -0500

    feat(workout-logs): eliminación con DELETE

--- src/routes/v1/workout-logs.routes.js
+++ src/routes/v1/workout-logs.routes.js
@@ -77,6 +77,16 @@ router.put('/:id', (req, res) => {
     res.status(200).json(workoutLogs[index]);
 });
 
-router.delete('/:id', (req, res) => { });
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const index = workoutLogs.findIndex(w => w.id === Number(id));
+
+    if (index === -1) {
+        return res.status(404).json({ error: 'Sesión no encontrada' });
+    }
+
+    workoutLogs.splice(index, 1);
+    res.status(200).json({ message: 'Sesión eliminada correctamente' });
+});
 
 module.exports = router;
