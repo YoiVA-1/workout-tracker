2b105a484f22e3f979e171f9fabd412316fedec4
YoiVA-1 <yoinervelez8@gmail.com>
2026-09-19 18:49:16 -0500

    fix(users): validación de parámetros, query strings y estados HTTP

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -15,7 +15,16 @@ let users = [
 
 // GET /v1/users
 router.get('/', (req, res) => {
-  res.status(200).json(users);
+  const { search } = req.query;
+  let result = users;
+
+  if (search) {
+    result = result.filter(u =>
+      u.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  res.status(200).json(result);
 });
 
 // GET /v1/users/:id
